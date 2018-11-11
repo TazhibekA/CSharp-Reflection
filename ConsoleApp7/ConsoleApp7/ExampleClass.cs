6d6e6ec0a72b572213ce2c56ac471c0ab137fcb9
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2018-11-12 00:44:00 +0600

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/ExampleClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp7
+{
+    public class ExampleClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public ExampleClass(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Show() {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+        }
+
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecondTask();
+        }
+
+        public static void FirstTask() {
+
+            Type console = typeof(Console);
+            MethodInfo[] methodInfos = console.GetMethods();
+            int counter = 1;
+            foreach (MethodInfo i in methodInfos)
+            
+            {
+                Console.Write(counter +". "+ i.ReturnType.Name + "    " + i.Name + "(");
+
+                ParameterInfo[] parameterInfos = i.GetParameters();
+
+                for (int j = 0; j < parameterInfos.Length; j++)
+                {
+                    Console.Write(parameterInfos[j].ParameterType.Name + " " + parameterInfos[j].Name);
+                    if (j + 1 < parameterInfos.Length) Console.Write(", ");
+                }
+                Console.WriteLine(")");
+                counter++;
+            }
+
+        
+        }
+
+        public static void SecondTask()
+        {
+
+            ExampleClass exampleClass = new ExampleClass("Aknur",18);
+
+            Type exampleClassType = typeof(ExampleClass);
+
+            Console.WriteLine("Methods of {0}",exampleClassType.Name);
+            PropertyInfo[] propertyInfos = exampleClassType.GetProperties();
+
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+               Console.WriteLine(propertyInfo.Name+"   "+ propertyInfo.PropertyType.Name + ", value:" + propertyInfo.GetValue(exampleClass, null));
+            }
+
+        }
+    }
+}
